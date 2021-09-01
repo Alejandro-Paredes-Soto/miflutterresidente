e4e586c39ec8a7790add118f6b83b59bbf755280
Saúl Ramírez <desarrollo.dostop@gmail.com>
2021-09-01 11:17:41 -0500

    Protegiendo archivo de constantes

--- lib/src/providers/constantes_provider.dart
+++ lib/src/providers/constantes_provider.dart
@@ -1,3 +1,3 @@
 final String _urlProd = 'https://dostop.mx/dostop/WebService';
-final String _urlTest = 'https://dostopmx.online/dostop/WebService';
+//final String _urlTest = 'https://dostopmx.online/dostop/WebService';
 final String urlApp = _urlProd;
