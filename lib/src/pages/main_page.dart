71809cae31e96e57e3bceb1fa6cd1e21fb7bff19
Saúl Ramírez <desarrollo.dostop@gmail.com>
2021-09-09 12:43:13 -0500

    Traslado de ui a home page.

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -1,22 +1,12 @@
-import 'package:dostop_v2/src/pages/areas_comunes_page.dart';
-import 'package:dostop_v2/src/pages/avisos_page.dart';
-import 'package:dostop_v2/src/pages/emergencia_page.dart';
-import 'package:dostop_v2/src/pages/estado_de_cuenta_page.dart';
-import 'package:dostop_v2/src/pages/home_page.dart';
-import 'package:dostop_v2/src/pages/mi_casa_page.dart';
-import 'package:dostop_v2/src/pages/promociones_page.dart';
-import 'package:dostop_v2/src/pages/visitantes_frecuentes_page.dart';
-import 'package:dostop_v2/src/pages/visitas_page.dart';
 import 'package:dostop_v2/src/providers/login_validator.dart';
-import 'package:dostop_v2/src/providers/login_provider.dart';
 import 'package:dostop_v2/src/push_manager/push_notification_manager.dart';
 import 'package:dostop_v2/src/utils/dialogs.dart';
 import 'package:dostop_v2/src/utils/preferencias_usuario.dart';
 import 'package:dostop_v2/src/utils/utils.dart' as utils;
 
-import 'package:flutter_svg/flutter_svg.dart';
-
 import 'package:flutter/material.dart';
+
+import 'home_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -24,15 +14,11 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
-  int _indexItem = 0;
-  final _pageController = PageController(keepPage: true);
   final _prefs = PreferenciasUsuario();
-  final _loginProvider = LoginProvider();
   final pushManager = PushNotificationsManager();
   final _validaSesion = LoginValidator();
   final GlobalKey<NavigatorState> navigatorKey =
       new GlobalKey<NavigatorState>();
-  final GlobalKey _navKey = GlobalKey();
 
   @override
   void initState() {
@@ -108,319 +94,9 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: navigatorKey,
-      body: _creaBody(),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Flexible(
-                  child: AnimatedContainer(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: _indexItem == 0
-                      ? utils.colorPrincipal
-                      : Colors.transparent,
-                ),
-                duration: Duration(milliseconds: 200),
-                height: 5,
-              )),
-              Flexible(
-                  child: AnimatedContainer(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: _indexItem == 1
-                      ? utils.colorPrincipal
-                      : Colors.transparent,
-                ),
-                duration: Duration(milliseconds: 200),
-                height: 5,
-              )),
-              Flexible(
-                  child: AnimatedContainer(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: _indexItem == 2
-                      ? utils.colorPrincipal
-                      : Colors.transparent,
-                ),
-                duration: Duration(milliseconds: 200),
-                height: 5,
-              )),
-              Flexible(
-                  child: AnimatedContainer(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(2),
-                  color: _indexItem == 3
-                      ? utils.colorPrincipal
-                      : Colors.transparent,
-                ),
-                duration: Duration(milliseconds: 200),
-                height: 5,
-              )),
-            ],
-          ),
-          _crearBNavBar(),
-        ],
-      ),
-    );
+    return Scaffold(key: navigatorKey, body: HomePage());
   }
 
-  Widget _creaBody() {
-    return PageView(
-      physics: NeverScrollableScrollPhysics(),
-      controller: _pageController,
-      onPageChanged: (index) {
-        setState(() {
-          if (_indexItem < 3) _indexItem = index;
-        });
-      },
-      children: <Widget>[
-        HomePage(
-          pageController: _pageController,
-        ),
-        VisitasPage(),
-        EmergenciasPage(),
-        AvisosPage(),
-        VisitantesFrecuentesPage(),
-        EstadoDeCuentaPage(),
-        AreasComunesPage(),
-        MiCasaPage(),
-        PromocionesPage(),
-      ],
-    );
-  }
-
-  Widget _crearBNavBar() {
-    return BottomNavigationBar(
-      key: _navKey,
-      type: BottomNavigationBarType.fixed,
-      showSelectedLabels: false,
-      showUnselectedLabels: false,
-      selectedItemColor: utils.colorPrincipal,
-      currentIndex: _indexItem,
-      onTap: (index) {
-        if (index == 3) {
-          _crearModalBottom(context);
-        } else {
-          _indexItem = index;
-          setState(() {
-            _pageController.jumpToPage(index);
-          });
-        }
-      },
-      items: [
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(utils.rutaIconoInicio,
-              color: Theme.of(context).iconTheme.color,
-              height: utils.tamanoIcoNavBar),
-          activeIcon: SvgPicture.asset(
-            utils.rutaIconoInicio,
-            height: utils.tamanoIcoNavBar,
-            color: utils.colorPrincipal,
-          ),
-          label: 'Inicio',
-        ),
-        BottomNavigationBarItem(
-            icon: SvgPicture.asset(utils.rutaIconoVisitas,
-                color: Theme.of(context).iconTheme.color,
-                height: utils.tamanoIcoNavBar),
-            activeIcon: SvgPicture.asset(utils.rutaIconoVisitas,
-                height: utils.tamanoIcoNavBar, color: utils.colorPrincipal),
-            label: 'Historial'),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(utils.rutaIconoEmergencia,
-              color: Theme.of(context).iconTheme.color,
-              height: utils.tamanoIcoNavBar),
-          activeIcon: SvgPicture.asset(
-            utils.rutaIconoEmergencia,
-            height: utils.tamanoIcoNavBar,
-            color: utils.colorPrincipal,
-          ),
-          label: 'Emergencias',
-        ),
-        BottomNavigationBarItem(
-          icon: Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: SvgPicture.asset(utils.rutaIconoMenu,
-                  height: 16, color: Theme.of(context).iconTheme.color)),
-          activeIcon: Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              child: SvgPicture.asset(utils.rutaIconoMenu,
-                  height: 16, color: utils.colorPrincipal)),
-          label: 'Más',
-        ),
-      ],
-    );
-  }
-
-  _crearModalBottom(BuildContext context) {
-    showModalBottomSheet<void>(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), topRight: Radius.circular(30))),
-        context: context,
-        builder: (BuildContext ctx) {
-          return Container(
-            padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-            child: Scrollbar(
-              child: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          flex: 5,
-                          child: _creaItemModal(
-                              icono: SvgPicture.asset(
-                                utils.rutaIconoAvisos,
-                                color: Theme.of(context).iconTheme.color,
-                                height: utils.tamanoIcoNavBar,
-                              ),
-                              titulo: 'Avisos',
-                              onTap: () {
-                                setState(() {
-                                  _pageController.jumpToPage(3);
-                                  _indexItem = 3;
-                                  Navigator.pop(ctx);
-                                });
-                              }),
-                        ),
-                        //FUTURO BOTÓN DE CONFIGURACIONES.
-                        // Flexible(
-                        //   flex: 1,
-                        //   child: IconButton(
-                        //     icon: Icon(Icons.settings),
-                        //     onPressed: (){},),
-                        // )
-                      ],
-                    ),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoVisitantesFrecuentes,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Visitantes Frecuentes',
-                        onTap: () {
-                          setState(() {
-                            _pageController.jumpToPage(4);
-                            _indexItem = 3;
-                            Navigator.pop(ctx);
-                          });
-                        }),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoEstadoDeCuenta,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Estados de Cuenta',
-                        onTap: () {
-                          setState(() {
-                            _pageController.jumpToPage(5);
-                            _indexItem = 3;
-                            Navigator.pop(ctx);
-                          });
-                        }),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoAreasComunes,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Áreas Comunes',
-                        onTap: () {
-                          setState(() {
-                            _pageController.jumpToPage(6);
-                            _indexItem = 3;
-                            Navigator.pop(ctx);
-                          });
-                        }),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoMiCasa,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Mi Casa',
-                        onTap: () {
-                          setState(() {
-                            _pageController.jumpToPage(7);
-                            _indexItem = 3;
-                            Navigator.pop(ctx);
-                          });
-                        }),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoPromociones,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Promociones',
-                        onTap: () {
-                          setState(() {
-                            _pageController.jumpToPage(8);
-                            _indexItem = 3;
-                            Navigator.pop(ctx);
-                          });
-                        }),
-                    _creaItemModal(
-                        icono: SvgPicture.asset(
-                          utils.rutaIconoCerrarSesion,
-                          color: Theme.of(context).iconTheme.color,
-                          height: utils.tamanoIcoNavBar,
-                        ),
-                        titulo: 'Cerrar Sesión',
-                        onTap: () {
-                          Navigator.pop(ctx);
-                          _cerrarSesion();
-                        }),
-                    _creaTerminosLegales()
-                  ],
-                ),
-              ),
-            ),
-          );
-        });
-  }
-
-  Widget _creaItemModal({Widget icono, String titulo, Function onTap}) {
-    return InkWell(
-      borderRadius: BorderRadius.circular(15),
-      child: Container(
-        height: 45,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(width: 10),
-            Container(
-                alignment: Alignment.center,
-                height: 45,
-                width: 35,
-                child: icono),
-            SizedBox(width: 20),
-            Flexible(
-                fit: FlexFit.tight,
-                child: Text(
-                  titulo,
-                  style: utils.estiloItemsModal(16),
-                  overflow: TextOverflow.fade,
-                  softWrap: false,
-                )),
-          ],
-        ),
-      ),
-      onTap: onTap,
-    );
-  }
 
   Widget _creaTerminosLegales() {
     return Container(
@@ -443,7 +119,6 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
                   url: 'https://dostop.mx/aviso-de-privacidad.html'),
             ),
           ),
-          //Flexible(fit: FlexFit.tight, child: Icon(Icons.phone, size: 36,),),
           Flexible(
             fit: FlexFit.tight,
             child: FlatButton(
@@ -462,44 +137,14 @@ class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
     );
   }
 
-  _cerrarSesion() {
-    creaDialogYesNoAlt(
-        context,
-        'Confirmar',
-        '¿Estás seguro de que deseas cerrar sesión?\n\nDejarás de recibir notificaciones de tus visitas.',
-        'Cerrar sesión',
-        'Cancelar', () {
-      Navigator.pop(context);
-      creaDialogProgress(context, 'Cerrando Sesión...');
-      _loginProvider.logout().then((logout) {
-        Navigator.pop(context);
-        if (logout) {
-          _prefs.borraPrefs();
-          print('${_prefs.usuarioLogged}');
-          Navigator.of(context).pushNamedAndRemoveUntil(
-              'login', (Route<dynamic> route) => false);
-        } else {
-          creaDialogSimple(
-              context,
-              '¡Ups! algo salió mal',
-              'No se pudo cerrar tu sesión, verifica tu conexión a internet',
-              'Aceptar',
-              () => Navigator.pop(context));
-        }
-      });
-    }, () => Navigator.pop(context));
-  }
-
   @override
   void deactivate() {
     super.deactivate();
-    // pushManager.detenerTimer();
   }
 
   @override
   void dispose() {
     super.dispose();
-    _pageController.dispose();
     WidgetsBinding.instance.removeObserver(this);
   }
 }
