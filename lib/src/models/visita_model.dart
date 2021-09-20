8189696e586c88655c575b6f797ade83e85c07fb
Saúl Ramírez <desarrollo.dostop@gmail.com>
2021-09-20 17:14:09 -0500

    Agregado valor de la fecha completa visita model.

--- lib/src/models/visita_model.dart
+++ lib/src/models/visita_model.dart
@@ -1,69 +1,71 @@
 import 'dart:convert';
 
-VisitaModel visitaModelFromJson(String str) => VisitaModel.fromJson(json.decode(str));
+VisitaModel visitaModelFromJson(String str) =>
+    VisitaModel.fromJson(json.decode(str));
 
 class VisitaModel {
-    String idVisitas;
-    String fechaEntrada;
-    String horaEntrada;
-    String fechaSalida;
-    String horaSalida;
-    String placa;
-    String color;
-    String marca;
-    String modelo;
-    String visitante;
-    String motivoVisita;
-    String imgPlaca;
-    String imgId;
-    String imgRostro;
-    int tipoVisita=1;
-    String codigo;
-    String estatus;
-    String reporte;
-    String noMolestar;
+  String idVisitas;
+  String fechaEntrada;
+  String horaEntrada;
+  String fechaSalida;
+  String horaSalida;
+  String placa;
+  String color;
+  String marca;
+  String modelo;
+  String visitante;
+  String motivoVisita;
+  String imgPlaca;
+  String imgId;
+  String imgRostro;
+  int tipoVisita = 1;
+  String codigo;
+  String estatus;
+  String reporte;
+  String noMolestar;
+  DateTime fechaCompleta;
 
+  VisitaModel({
+    this.idVisitas,
+    this.fechaEntrada,
+    this.horaEntrada,
+    this.fechaSalida,
+    this.horaSalida,
+    this.placa,
+    this.color,
+    this.marca,
+    this.modelo,
+    this.visitante,
+    this.motivoVisita,
+    this.imgPlaca,
+    this.imgId,
+    this.imgRostro,
+    this.tipoVisita,
+    this.codigo,
+    this.estatus,
+    this.reporte,
+    this.noMolestar,
+    this.fechaCompleta,
+  });
 
-    VisitaModel({
-        this.idVisitas,
-        this.fechaEntrada,
-        this.horaEntrada,
-        this.fechaSalida,
-        this.horaSalida,
-        this.placa,
-        this.color,
-        this.marca,
-        this.modelo,
-        this.visitante,
-        this.motivoVisita,
-        this.imgPlaca,
-        this.imgId,
-        this.imgRostro,
-        this.tipoVisita,
-        this.codigo,
-        this.estatus,
-        this.reporte,
-        this.noMolestar,
-    });
-
-    factory VisitaModel.fromJson(Map<String, dynamic> json) => VisitaModel(
-        idVisitas: json["idVisitas"],
-        fechaEntrada:json["fecha_entrada"],
-        horaEntrada:  json["hora_entrada"],
-        fechaSalida: json['fecha_salida'],
-        horaSalida: json["hora_salida"],
-        placa: json["placa"],
-        color: json["color"],
-        marca: json["marca"],
-        modelo: json["modelo"],
-        visitante: json["visitante"],
-        motivoVisita: json["motivo_visita"],
-        imgPlaca: json["img_placa"],
-        imgId: json["img_id"],
-        imgRostro: json["img_rostro"],
-        codigo: json["codigo"],
-        estatus: json["estatus"],
-        reporte: json["reporte"],
-        noMolestar: json["no_molestar"],
-    );
+  factory VisitaModel.fromJson(Map<String, dynamic> json) => VisitaModel(
+      idVisitas: json["idVisitas"],
+      fechaEntrada: json["fecha_entrada"],
+      horaEntrada: json["hora_entrada"],
+      fechaSalida: json['fecha_salida'],
+      horaSalida: json["hora_salida"],
+      placa: json["placa"],
+      color: json["color"],
+      marca: json["marca"],
+      modelo: json["modelo"],
+      visitante: json["visitante"],
+      motivoVisita: json["motivo_visita"],
+      imgPlaca: json["img_placa"],
+      imgId: json["img_id"],
+      imgRostro: json["img_rostro"],
+      codigo: json["codigo"],
+      estatus: json["estatus"],
+      reporte: json["reporte"],
+      noMolestar: json["no_molestar"],
+      fechaCompleta: DateTime.tryParse(json["fecha_completa"]??""));
 }
