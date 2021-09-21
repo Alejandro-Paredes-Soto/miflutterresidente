3e7965816a7c3a09653696b3752aa7a6a55f5dd7
Saúl Ramírez <desarrollo.dostop@gmail.com>
2021-09-21 12:47:29 -0500

    Personalización al contenedor elevado.

--- lib/src/widgets/elevated_container.dart
+++ lib/src/widgets/elevated_container.dart
@@ -3,9 +3,17 @@ import 'package:flutter/material.dart';
 class ElevatedContainer extends StatelessWidget {
   final Widget child;
   final EdgeInsetsGeometry padding;
+  final double opacity;
+  final double blurRadius;
+  final double offsetX;
+  final double offsetY;
   const ElevatedContainer({
-    this.child,
+    @required this.child,
     this.padding = const EdgeInsets.all(0),
+    this.opacity = 0.18,
+    this.blurRadius = 20.0,
+    this.offsetX = 3.0,
+    this.offsetY = 3.0,
     Key key,
   }) : super(key: key);
   @override
@@ -17,9 +25,9 @@ class ElevatedContainer extends StatelessWidget {
         color: Theme.of(context).cardColor,
         boxShadow: [
           BoxShadow(
-              color: Color.fromRGBO(0, 0, 0, 0.18),
-              blurRadius: 20,
-              offset: Offset(3, 3))
+              color: Color.fromRGBO(0, 0, 0, opacity),
+              blurRadius: blurRadius,
+              offset: Offset(offsetX, offsetY))
         ],
       ),
       child: child,
