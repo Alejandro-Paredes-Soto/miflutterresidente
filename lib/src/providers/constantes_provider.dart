fc8d80750392ccf4284c07a9baee8bd7008c3fce
AlejandroParedesSoto <alejandroparedessoto18@gmail.com>
2024-10-30 20:18:47 -0600

    cambios

--- lib/src/providers/constantes_provider.dart
+++ lib/src/providers/constantes_provider.dart
@@ -1,4 +1,4 @@
-final String _urlProd = 'https://dostop.mx/dostop/WebService';
+final String _urlProd = 'http://localhost/dostop/WebService';
 final String _urlApiP = 'https://dostop.mx/integracionpd/public/api/v1';
 //final String _urlTest = 'https://dostopmx.online/dostop/WebService';
 final String urlApp = _urlProd;
